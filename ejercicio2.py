bfaa7b10d5a62f2f74485dd5413a9c81652dcd5b
st3v3n030707 <safloreso@uamv.edu.ni>
2025-04-09 14:31:40 -0600

    calcular salario de trabajador

---
+++ ejercicio2.py
@@ -0,0 +1,5 @@
+print ("Quieres saber tus ganacis totales en bases a tus horas laborales?: ")
+salario = float(input("Dime tu salario: "))
+horas = float(input("Dime tus horas trabajadas: "))
+total = salario * horas
+print(f"Tu salario total dependiendo de las horas trabajadas es de: {total}")
